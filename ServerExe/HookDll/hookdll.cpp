bb045ce2c716dc3b52ddf5eb36565eba958f7985
Peter Åstrand <astrand@cendio.com>
2005-08-30 11:17:52 +0000

    Fixed compiler warnings: Removed unused variables. Using %p for converting HWND to hex.
    Implemented SETSTATE1.
    
    
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/trunk/seamlessrdp@1001 423420c4-83ab-492f-b58f-81f9feb106b5

--- ServerExe/HookDll/hookdll.cpp
+++ ServerExe/HookDll/hookdll.cpp
@@ -92,15 +92,6 @@ LRESULT CALLBACK CallWndProc( int nCode, WPARAM wParam, LPARAM lParam )
     HWND windowHandle2 = NULL;
     char result[ 255 ] = { ""
                          };
-    char strWindowId[ 25 ];
-    char type[ 25 ];
-    
-    LONG b, t, l, r;
-    char strX[ 5 ];
-    char strY[ 5 ];
-    char strW[ 5 ];
-    char strH[ 5 ];
-    
     CWPSTRUCT *details = ( CWPSTRUCT * ) lParam;
     CREATESTRUCT *cs = ( CREATESTRUCT * ) details->lParam;
     LONG dwStyle = GetWindowLong( details->hwnd, GWL_STYLE );
@@ -118,8 +109,8 @@ LRESULT CALLBACK CallWndProc( int nCode, WPARAM wParam, LPARAM lParam )
             break;
             
         snprintf( result, sizeof( result ),
-                  "POSITION1,0x%x,%d,%d,%d,%d,0x%x",
-                  ( int ) details->hwnd,
+                  "POSITION1,0x%p,%d,%d,%d,%d,0x%x",
+                  details->hwnd,
                   rect->left, rect->top,
                   rect->right - rect->left,
                   rect->bottom - rect->top,
@@ -147,7 +138,7 @@ LRESULT CALLBACK CallWndProc( int nCode, WPARAM wParam, LPARAM lParam )
             
         if ( !( wp->flags & SWP_NOZORDER ) ) {
             snprintf( result, sizeof( result ),
-                      "ZCHANGE1,0x%x,0x%x,0x%x\n",
+                      "ZCHANGE1,0x%p,0x%p,0x%x\n",
                       details->hwnd,
                       wp->flags & SWP_NOACTIVATE ? wp->hwndInsertAfter : 0,
                       0 );
@@ -160,7 +151,7 @@ LRESULT CALLBACK CallWndProc( int nCode, WPARAM wParam, LPARAM lParam )
         case WM_CREATE:
         if ( cs->style & WS_DLGFRAME ) {
             snprintf( result, sizeof( result ),
-                      "CREATE1,0x%x,0x%x\n",
+                      "CREATE1,0x%p,0x%x\n",
                       details->hwnd, 0 );
             buffer = result;
         }
@@ -170,7 +161,7 @@ LRESULT CALLBACK CallWndProc( int nCode, WPARAM wParam, LPARAM lParam )
         case WM_DESTROY:
         if ( dwStyle & WS_DLGFRAME ) {
             snprintf( result, sizeof( result ),
-                      "DESTROY1,0x%x,0x%x\n",
+                      "DESTROY1,0x%p,0x%x\n",
                       details->hwnd, 0 );
             buffer = result;
         }
@@ -206,104 +197,26 @@ LRESULT CALLBACK CbtProc( int nCode, WPARAM wParam, LPARAM lParam )
     HWND windowHandle = NULL;
     char result[ 255 ] = { ""
                          };
-    char strWindowId[ 25 ];
-    char type[ 25 ];
-    
-    
-    LONG b, t, l, r;
-    char strW[ 5 ];
-    char strY[ 5 ];
-    char strX[ 5 ];
-    char strH[ 5 ];
-    RECT rect;
-    
-    int i = 0; //tmp
-    
     switch ( nCode ) {
         case HCBT_MINMAX:
         
-        windowHandle = ( HWND ) wParam;
-        //get win name
-        GetWindowText( windowHandle, windowTitle, 150 );
+        if ( ( LOWORD( lParam ) == SW_SHOWMINIMIZED )
+                || ( LOWORD( lParam ) == SW_MINIMIZE ) ) {
+            MessageBox( 0, "Minimizing windows is not allowed in this version. Sorry!", "SeamlessRDP", MB_OK );
+            return 1;
+        }
         
-        //get an id for it
-        itoa( ( int ) windowHandle, strWindowId, 10 );
+        GetWindowText( ( HWND ) wParam, windowTitle, 150 );
         
-        //get operation type(min,max). if max, do not clip at all,if min use window's previous coords
-        //codes are:
-        
-        // SW_HIDE= 0  SW_SHOWNORMAL=1  SW_NORMAL=1  SW_SHOWMINIMIZED=2  SW_SHOWMAXIMIZED=3  SW_MAXIMIZE=3
-        // SW_SHOWNOACTIVATE=4  SW_SHOW=5  SW_MINIMIZE=6  SW_SHOWMINNOACTIVE=7  SW_SHOWNA=8  SW_RESTORE=9
-        // SW_SHOWDEFAULT=10  SW_FORCEMINIMIZE=11  SW_MAX=11
-        
-        itoa( ( int ) lParam, type, 10 );
-        
-        //get coords
-        GetWindowRect( windowHandle, &rect );
-        b = rect.bottom;
-        t = rect.top;
-        l = rect.left;
-        r = rect.right;
-        ltoa( b - t, strW, 10 );
-        ltoa( t, strY, 10 );
-        ltoa( r - l, strH, 10 );
-        ltoa( l, strX, 10 );
-        
-        //get name
-        GetWindowText( windowHandle, windowTitle, 150 );
-        
-        ////setup return string
-        strcat( result, "MSG=HCBT_MINMAX;OP=4;" );
-        
-        // SW_SHOWNOACTIVATE=4  SW_SHOW=5  SW_MINIMIZE=6  SW_SHOWMINNOACTIVE=7  SW_SHOWNA=8  SW_RESTORE=9
-        // SW_SHOWDEFAULT=10  SW_FORCEMINIMIZE=11  SW_MAX=11
-        strcat( result, "ID=" );
-        strcat( result, strWindowId );
-        strcat( result, ";" );
-        strcat( result, "TITLE=" );
-        strcat( result, windowTitle );
-        strcat( result, ";" );
-        strcat( result, "X=" );
-        strcat( result, strX );
-        strcat( result, ";" );
-        strcat( result, "Y=" );
-        strcat( result, strY );
-        strcat( result, ";" );
-        strcat( result, "H=" );
-        strcat( result, strH );
-        strcat( result, ";" );
-        strcat( result, "W=" );
-        strcat( result, strW );
-        strcat( result, ";" );
-        strcat( result, "TYPE=" );
-        strcat( result, type );
-        strcat( result, "." );
-        
-        //-------------------------------------------------------------------------------------------------
-        // code to prevent minimising windows (can be removed once minimise has been implemented)
-        //i = (int)lParam;
-        //if (i==0 || i==2 || i==6 || i==7 || i==8 || i==11)
-        //if ( i==2 || i==6 )
-        //{
-        // MessageBox(0,"Minimising windows is not allowed in this version. Sorry!","TS Window Clipper", MB_OK);
-        // return 1;
-        //}
-        //-----------------------------------------------------------------------------------------
-        
-        //-------------------------------------------------------------------------------------------------
-        // code to prevent maximising windows (can be removed once maximise has been implemented)
-        //i = (int)lParam;
-        //if (i==3 || i==9 || i==11)
-        //if (i==3 || i==11)
-        //{
-        // MessageBox(0,"Maximising windows is not allowed in this version. Sorry!","TS Window Clipper", MB_OK);
-        // return 1;
-        //}
-        //-----------------------------------------------------------------------------------------
-        
+        snprintf( result, sizeof( result ),
+                  "SETSTATE1,0x%p,%s,0x%x,0x%x\n",
+                  ( HWND ) wParam,
+                  windowTitle,
+                  LOWORD( lParam ),
+                  0 );
         buffer = result;
-        
         break;
+        
         
         default:
         break;
