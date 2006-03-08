b7ee4d63954907ca053bd956920f81b113a5a70c
Peter Åstrand <astrand@cendio.com>
2006-03-08 09:41:55 +0000

    Mark DllMain as extern "C".
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/trunk/seamlessrdp@1061 423420c4-83ab-492f-b58f-81f9feb106b5

--- ServerExe/HookDll/hookdll.cpp
+++ ServerExe/HookDll/hookdll.cpp
@@ -49,7 +49,7 @@ void SendDebug( char *format, ... )
 
 
 
-BOOL APIENTRY DllMain( HINSTANCE hinstDLL, DWORD ul_reason_for_call, LPVOID lpReserved )
+extern "C" BOOL APIENTRY DllMain( HINSTANCE hinstDLL, DWORD ul_reason_for_call, LPVOID lpReserved )
 {
     switch ( ul_reason_for_call ) {
         case DLL_PROCESS_ATTACH: {
